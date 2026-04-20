0014ae7667e0bcf96e17044737323a306217799b
owm <enless143@gmail.com>
2026-04-20 07:58:27 +0200

    Fixed a typo in utils that caused an error upon resizing the image in the transforms pipeline

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import json
 import torch
 import torch.nn as nn
 from torchvision import transforms, models
+from PIL import Image
 
 PATH_MODEL_CONFIG   = 'xray_classifier.json'
 PATH_MODEL          = 'xray_classifier.pth'
@@ -23,10 +24,10 @@ def load_your_model(model_path:str=PATH_MODEL, config_path:str=PATH_MODEL_CONFIG
     Returns:
         model: Loaded model
         config: Configuration dict
-        transform: Transform pipeline
+        preprocess: Transforms pipeline
     
     Example Usage:
-        model, config, transform = load_saved_model(model_path, config_path)
+        model, config, preprocess = load_saved_model(model_path, config_path)
     '''    
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -35,20 +36,33 @@ def load_your_model(model_path:str=PATH_MODEL, config_path:str=PATH_MODEL_CONFIG
 
     # Create model
     model = models.resnet18(weights=None)
-    model.fc = nn.Linear(model.fc.in_features, config['num_classes'])
+    model.fc = nn.Linear(model.fc.in_features, len(config['classes']))
     
     # Load weights
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
     
-    # Define transform
-    transform = transforms.Compose([
-        transforms.Resize((config['input_size'], config['input_size'])),
+    # Define preprocess func
+    preprocess = transforms.Compose([
+        transforms.Resize(config['input_size']),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], 
                             std=[0.229, 0.224, 0.225])
     ])
     
-    return model, config, transform
+    return model, config, preprocess
 
+def predict(img:Image.Image, model, config, preprocess):
+	with torch.no_grad():
+		probs = torch.softmax(model(preprocess(img).unsqueeze(0)), dim=1)[0]
+	return {'class': config['classes'][probs.argmax()], 'confidence': float(probs.max()), 'probs': probs}
+
+def init():
+	pass    #TODO: maybe have model object and config loaded internally
+	
+if __name__=='__main__':
+	pass
+else:
+	init()
+	
