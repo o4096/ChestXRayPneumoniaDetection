825f80970cf784a17731589fa18b3128f85ddd6f
owm <enless143@gmail.com>
2026-04-24 20:14:27 +0300

    split notebook for a more decoupled pipeline

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ PATH_MODEL_CONFIG   = 'xray_classifier.json'
 PATH_MODEL          = 'xray_classifier.pth'
 
 def load_config(path:str)->dict:
-	with open(path, 'r') as f:
-		config= json.load(f)
-	return config
+    with open(path, 'r') as f:
+        config= json.load(f)
+    return config
 
 def load_your_model(model_path:str=PATH_MODEL, config_path:str=PATH_MODEL_CONFIG, device=None):
     '''
@@ -28,7 +28,7 @@ def load_your_model(model_path:str=PATH_MODEL, config_path:str=PATH_MODEL_CONFIG
     
     Example Usage:
         model, config, preprocess = load_saved_model(model_path, config_path)
-    '''    
+    '''
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -54,15 +54,15 @@ def load_your_model(model_path:str=PATH_MODEL, config_path:str=PATH_MODEL_CONFIG
     return model, config, preprocess
 
 def predict(img:Image.Image, model, config, preprocess):
-	with torch.no_grad():
-		probs = torch.softmax(model(preprocess(img).unsqueeze(0)), dim=1)[0]
-	return {'class': config['classes'][probs.argmax()], 'confidence': float(probs.max()), 'probs': probs}
+    with torch.no_grad():
+        probs = torch.softmax(model(preprocess(img).unsqueeze(0)), dim=1)[0]
+    return {'class': config['classes'][probs.argmax()], 'confidence': float(probs.max()), 'probs': probs}
 
 def init():
-	pass    #TODO: maybe have model object and config loaded internally
-	
+    pass    #TODO: maybe have model object and config loaded internally (experimental)
+
 if __name__=='__main__':
-	pass
+    pass
 else:
-	init()
-	
+    init()
+
